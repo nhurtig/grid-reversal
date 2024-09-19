3122f7204551612a06037b924bfc3e2c77928350
Nat Hurtig <nhurtig@acm.org>
2024-09-19 15:35:14 -0700

    Direct output to .dot, .pdf

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Rewriter
+import System.Process (callProcess)
 import Viz (toViz)
 import Word (showWord, stringToWord)
 import WordReversal (reverseString)
@@ -15,10 +16,27 @@ handleInput :: String -> IO ()
 handleInput str =
   let word = stringToWord str
    in let (rWord, g) = reverseWord word
-       in do
-            putStrLn "Grid graphviz is:"
-            putStrLn $ toViz g
-            putStrLn "Direct reversed word is:"
-            putStrLn $ reverseString str
-            putStrLn "Grid reversed word is:"
-            putStrLn $ showWord rWord
+       in let name = (str ++ "_" ++ showWord rWord)
+           in let filename = name ++ ".dot"
+               in let pdfFilename = name ++ ".pdf"
+                   in do
+                        -- Write to the file
+                        writeFile filename (toViz g)
+
+                        -- Call dot to generate a PDF
+                        tryDot filename pdfFilename
+
+                        -- case exitCode of
+                        putStrLn $ "Attempted to generate PDF at: " ++ pdfFilename
+                        --   ExitFailure _ -> putStrLn "Failed to generate PDF -- you might not have graphviz."
+
+                        putStrLn $ "Grid graphviz is written to: " ++ filename
+                        putStrLn "Direct reversed word is:"
+                        putStrLn $ reverseString str
+                        putStrLn "Grid reversed word is:"
+                        putStrLn $ showWord rWord
+
+tryDot :: FilePath -> FilePath -> IO ()
+tryDot dotFile pdfFile = do
+  callProcess "dot" ["-Tpdf", dotFile, "-o", pdfFile]
+
