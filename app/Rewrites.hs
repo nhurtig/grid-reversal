c7d5e3679356e2deeb8bdb62cb7a585193d18541
Nat Hurtig <nhurtig@acm.org>
2024-09-18 20:02:45 -0700

    all 3 rewrite rules done!

--- app/Rewrites.hs
+++ app/Rewrites.hs
@@ -1,5 +1,5 @@
 -- This module sets up what reversal rules are
-module Reversal where
+module Rewrites where
 
 import Grid
 import Numeric.Natural
@@ -51,9 +51,9 @@ makeVertexCancel _ _ = \_ _ -> Nothing -- Has to be neg, then pos!
 -- Creates the face from Def 3.2.1 corresponding to sigma cancel, using
 -- the given vertices
 makeVertexCancelFace :: Vertex -> Vertex -> Vertex
-makeVertexCancelFace oldBotLef oldTopRig =
+makeVertexCancelFace oldTopLef oldTopRig =
   let botRight = Vertex (Just $ VEdge oldTopRig Nothing) Nothing
-   in Vertex (up oldBotLef) (Just $ HEdge botRight Nothing)
+   in Vertex (up oldTopLef) (Just $ HEdge botRight Nothing)
 
 --
 -- SECOND RULE (sigma swap)
@@ -87,7 +87,7 @@ vertexSwap 0 v = do
 vertexSwap i v = do
   -- go to next vertex
   nextV <- fst <$> nextInter v
-  vertexCancel (i - 1) nextV
+  vertexSwap (i - 1) nextV
 
 -- If the generators do swap, returns a face that points to
 -- the given vertex out of the vertices; otherwise Nothing
@@ -98,6 +98,48 @@ makeVertexSwap (l1, False) (l2, True)
 makeVertexSwap _ _ = \_ _ -> Nothing -- Has to be neg, then pos!
 
 makeVertexSwapFace :: Natural -> Natural -> Vertex -> Vertex -> Vertex
-makeVertexSwapFace l1 l2 oldBotLef oldTopRig =
-  let botRight = Vertex (Just $ VEdge oldTopRig $ Just l2) Nothing
-   in Vertex (up oldBotLef) (Just $ HEdge botRight $ Just l1)
+makeVertexSwapFace l1 l2 oldTopLef oldTopRig =
+  let botRight = Vertex (Just $ VEdge oldTopRig $ Just l1) Nothing
+   in Vertex (up oldTopLef) (Just $ HEdge botRight $ Just l2)
+
+--
+-- THIRD RULE (yang-baxter)
+--
+
+-- Attempts to apply the third reversal rule
+-- on the given index starting from the bottom
+-- left corner and taking the "outside", intermediary
+-- route (right if you can, o/w up)
+yangBaxter :: Natural -> Rewrite
+yangBaxter i (Grid v) = do
+  newV <- vertexYB i v
+  Just $ Grid newV
+
+-- Does the above function, but on a vertex instead of a grid; returns
+-- the new vertex
+vertexYB :: Natural -> Vertex -> Maybe Vertex
+vertexYB 0 v = do
+  (v1, mGen1) <- nextInter v
+  (v2, mGen2) <- nextInter v1
+  gen1 <- mGen1
+  gen2 <- mGen2
+  makeVertexYB gen1 gen2 v v2
+vertexYB i v = do
+  -- go to next vertex
+  nextV <- fst <$> nextInter v
+  vertexYB (i - 1) nextV
+
+-- If the generators do yang-baxter, returns a face that points to
+-- the given vertex out of the vertices; otherwise Nothing
+makeVertexYB :: Generator -> Generator -> Vertex -> Vertex -> Maybe Vertex
+makeVertexYB (l1, False) (l2, True)
+  | metric l1 l2 /= 1 = \_ _ -> Nothing -- indices aren't adjacent!
+  | otherwise = (Just .) . makeVertexYBFace l1 l2
+makeVertexYB _ _ = \_ _ -> Nothing -- Has to be neg, then pos!
+
+makeVertexYBFace :: Natural -> Natural -> Vertex -> Vertex -> Vertex
+makeVertexYBFace l1 l2 oldTopLef oldTopRig =
+  let midRig = Vertex (Just $ VEdge oldTopRig $ Just l1) Nothing
+   in let botRig = Vertex (Just $ VEdge midRig $ Just l2) Nothing
+       in let botMid = Vertex Nothing (Just $ HEdge botRig $ Just l1)
+           in Vertex (up oldTopLef) (Just $ HEdge botMid $ Just l2)
